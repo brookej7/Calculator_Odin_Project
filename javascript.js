d2b60b4619ed871d87a411dc48cbbe848b006ba5
brookej7 <brooke.e.jones@bigpond.com>
2024-05-05 12:14:45 +1000

    Implement operate function to perform operation
    Created operate function which will select the
    correct operate function and return the output
    for a given operator.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let NUM1;
+let NUM2;
+let OPERATOR;
+
 function add(num1, num2) {
 
     return num1 + num2;
@@ -12,6 +16,12 @@ function subtract(num1, num2) {
 
 function divide(num1, num2) {
 
+    if (num2 == 0) {
+
+        return 'error'
+
+    }
+
     return num1 / num2;
 
 }
@@ -19,5 +29,31 @@ function divide(num1, num2) {
 function multiply(num1, num2) {
 
     return num1 * num2;
+
+}
+
+function operate(num1, num2, operator) {
+
+    let result;
+
+    switch(operator) {
+        case '+':
+            result = add(num1, num2);
+            break
+
+        case '-':
+            result = subtract(num1, num2);
+            break
+
+        case '/':
+            result = divide(num1, num2);
+            break
+
+        case '*':
+            result = multiply(num1, num2);
+            break
+    }
+
+    return result
 
 }
